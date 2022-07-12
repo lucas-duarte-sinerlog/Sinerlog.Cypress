ea1fbe545663205a25123132992feab9bbfdafa2
Lucas Duarte <lucas.duarte@sinerlog.global>
2022-07-12 12:05:29 -0300

    Wallet feature

--- Cypress.SinerlogSafetyTracker/cypress/e2e/Package/Package.ts
+++ Cypress.SinerlogSafetyTracker/cypress/e2e/Package/Package.ts
@@ -2,7 +2,10 @@ import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 import { TenantToken } from "../Tenant/TenantToken.enum";
 import { v4 as uuidv4 } from 'uuid';
 import { PackageWebClient } from "./PackageWebClient";
-import { Request } from "../../../../src/utils/Request";
+import { CyRequest } from "../../../../src/utils/CyRequest";
+
+const request = new CyRequest();
+
 Given(/^I want to create a package$/, () => { });
 
 Then(/^the following tenant exists$/, () => {
@@ -14,7 +17,7 @@ Then(/^the following tenant exists$/, () => {
         },
         failOnStatusCode: false
     }).then((response) => {
-        Request.status = response.status
+        request.status = response.status
     });
 });
 
@@ -22,15 +25,15 @@ Then(/^the following tenant exists$/, () => {
 When(/^i set a default payload$/, () => {
     cy.fixture('SinerlogSafetyTracker/Package/add-package').then(jsonBody => {
         jsonBody.orderNumber = uuidv4()
-        Request.jsonBody = jsonBody
+        request.jsonBody = jsonBody
     })
 });
 
 
 When(/^send a create request$/, () => {
-    PackageWebClient.Add(Request)
+    PackageWebClient.Add(request)
 });
 
 Then(/^the package should be created$/, () => {
-    expect(Request.status).to.be.equals(201)
+    expect(request.status).to.be.equals(201)
 })
